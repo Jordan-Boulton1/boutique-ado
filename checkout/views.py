8659e83296e3d74d2f5bfaf3b4729453475f665f
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2024-06-26 12:38:25 +0100

    feat: added stripe elements.js

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PVtfaC4z3cDsr7iMVhvgFAySEiPpmEyc5ssHRnATRH5KKrEKJbqwnvuVL1oe5CcjRKXaG88M2CDdEl4FeRxFly100LCdpb5rL',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
