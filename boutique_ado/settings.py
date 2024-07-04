dc3999d9660271f42156dd9c06cdb5f53968aed3
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2024-07-04 12:11:24 +0100

    turn off debug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['jb-ci-boutique-ado-0fd50c244260.herokuapp.com', '127.0.0.1']
 
